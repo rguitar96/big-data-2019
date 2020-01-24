c7939f02095343be134320da91bd92686696ef2d
unknown <rodrigo.pueblas@hotmail.com>
2020-01-24 20:18:16 +0100

    Final page started

--- visualization/Final/app.R
+++ visualization/Final/app.R
@@ -8,7 +8,10 @@ library("KernSmooth")
 library("data.table")
 library("sp")
 library("rgdal")
-library("raster")
+library(tm)
+library(wordcloud)
+library(wordcloud2)
+library(memoise)
 
 
 languages = data.frame(
@@ -39,16 +42,57 @@ languages = data.frame(
            "ru", 
            "sv"))
 
+
 data <- read.csv("../data/clean-twitter-data.csv")
-data$Date
+data$Date <- as.Date(data$Date)
+
+#Using "memoise" to automatically cache the results
+getTermMatrix <- memoise(function(langName, startDate, endDate) {
+  
+  lang = languages %>% filter(name == langName)
+  
+  
+  text <- data %>%
+    filter(Tweet.language..ISO.639.1. == lang$code) %>% 
+    filter(Date >= startDate) %>% 
+    filter(Date <= endDate)
+  
+  docs = Corpus(VectorSource(text$Tweet.content))
+  
+  toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+  docs <- tm_map(docs, toSpace, "/")
+  docs <- tm_map(docs, toSpace, "@")
+  docs <- tm_map(docs, toSpace, "\\|")
+  
+  # Convert the text to lower case
+  docs <- tm_map(docs, content_transformer(tolower))
+  # Remove numbers
+  docs <- tm_map(docs, removeNumbers)
+  # Remove language common stopwords
+  docs <- tm_map(docs, removeWords, stopwords(tolower(lang$name)))
+  # Remove punctuations
+  docs <- tm_map(docs, removePunctuation)
+  # Eliminate extra white spaces
+  docs <- tm_map(docs, stripWhitespace)
+  # Remove your own stop word
+  # specify your stopwords as a character vector
+  docs <- tm_map(docs, removeWords, c("https", "tco", "...", "com"))
+  
+  dtm <- TermDocumentMatrix(docs)
+  m <- as.matrix(dtm)
+  v <- sort(rowSums(m),decreasing=TRUE)
+  d <- data.frame(word = names(v),freq=v)
+})
+
 
 
 ui <- dashboardPage(
-  dashboardHeader(  ),
+  dashboardHeader(),
+  # SIDE BAR
   dashboardSidebar(
     textInput("wordFilter", "Filter by word: ", value = ""),
     selectInput("lang", "Choose a language:",
-                choices = languages$code),
+                choices = languages$name),
     sliderInput("opacity", label = "Opacity",
                 min = 0, max = 1,
                 value = 0.8),
@@ -58,27 +102,39 @@ ui <- dashboardPage(
     sliderInput("bandwidth", label = "Bandwidth",
                 min = 0.000001, max = 0.5,
                 value = 0.001),
+    sliderInput("dateSlider", label = h3("Date"),
+                min = min(data$Date), max = max(data$Date),
+                value = c(min(data$Date),max(data$Date))),
     actionButton("update", "Change")
   ),
+  # BODY, BACKGROUND
   dashboardBody(
     tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
     leafletOutput("map"),
     
+    absolutePanel(id="controls",
+                  style="z-index:500;",
+                  class = "panel panel-default",
+                  draggable = TRUE,
+                  wordcloud2Output("plot"))
   )
 )
 
 
 server <- function(input, output) {
   
+  # HEATMAP
   output$map <- renderLeaflet({
+    lang = languages %>% filter(name == input$lang)
+    
     dat <- data %>% 
       filter(Latitude < 44) %>% 
       filter(Latitude > 35) %>% 
       filter(Longitude > -12) %>% 
       filter(Longitude < 7) %>% 
-      filter(Tweet.language..ISO.639.1. == input$lang) %>% 
-      #filter(Date >= input$dateSlider[1]) %>%
-      #filter(Date <= input$dateSlider[2]) %>% 
+      filter(Tweet.language..ISO.639.1. == lang$code) %>% 
+      filter(Date >= input$dateSlider[1]) %>%
+      filter(Date <= input$dateSlider[2]) %>% 
       filter(grepl(tolower(input$wordFilter),tolower(Tweet.content)))
     
     dat <- data.table(dat)
@@ -94,7 +150,7 @@ server <- function(input, output) {
     leaflet() %>% addTiles() %>% 
       addProviderTiles(providers$CartoDB.DarkMatter) %>% 
       addRasterImage(KernelDensityRaster, 
-                    colors = palRaster, 
+                     colors = palRaster, 
                      opacity = input$opacity) %>%
       addLegend(pal = palRaster, 
                 values = KernelDensityRaster@data@values, 
@@ -104,6 +160,24 @@ server <- function(input, output) {
                     , lat1 = 35
                     , lng2 = 7
                     , lat2 = 44 )
+  })
+  
+  # WORDCLOUD
+  terms <- reactive({
+    # Change when the "update" button is pressed...
+    input$update
+    # ...but not for anything else
+    isolate({
+      withProgress({
+        setProgress(message = "Processing corpus...")
+        getTermMatrix(input$lang, input$dateSlider[1], input$dateSlider[2])
+      })
+    })
+  })
+  
+  
+  output$plot <- renderWordcloud2({
+    wordcloud2(data=terms(), size = 0.7, shape = 'pentagon')
   })
 }
 
